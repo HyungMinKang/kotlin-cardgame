b3e38c5386d97295815cd29e2840faf1231a211e
ramkid91 <ramkid91@gmail.com>
2022-02-24 16:19:57 +0900

    Fix: Reallocate Branch

--- app/src/main/kotlin/Card.kt
+++
@@ -1,9 +0,0 @@
-class Card(fruitName:String, cardValue:Int) {
-    private val cardValue = CardValue(cardValue)
-    private val cardShape = CardShape(fruitName)
-
-    override fun toString(): String {
-        return "$cardShape$cardValue"
-    }
-}
-
